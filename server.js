5dfd421f138516f88e5d08fba8e94dd71dbdbe09
siriuzz <elianmatosd03+github@gmail.com>
2023-09-14 15:26:37 -0400

    log de process.env

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-
+console.log(process.env);
 /* eslint-disable max-len */
-const {MONGO_DB_USR, MONGO_DB_PWD, MONGO_DB_HOST, MONGO_DB_PORT, MONGO_DB_NAME} =
+const { MONGO_DB_USR, MONGO_DB_PWD, MONGO_DB_HOST, MONGO_DB_PORT, MONGO_DB_NAME } =
   process.env;
 const credentials = MONGO_DB_USR ? `${MONGO_DB_USR}:${MONGO_DB_PWD}@` : '';
 const mongoURI = `mongodb://${credentials}${MONGO_DB_HOST}:${MONGO_DB_PORT}/${MONGO_DB_NAME}`;
 
 mongoose
-    .connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-      app.listen(port, (arg) => {
-        console.log(`Server started @ ${port}.`);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, (arg) => {
+      console.log(`Server started @ ${port}.`);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
